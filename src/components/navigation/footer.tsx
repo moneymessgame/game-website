bb431e5619d1b04d64589b8d57aadf0377c5abed
Irine7 <irine7@yandex.ru>
2024-08-24 16:27:28 +0200

    refactor: add footer

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -14,8 +14,8 @@ const Footer = () => {
 			<div className="hidden lg:block absolute -top-1/3 -right-1/4 bg-primary w-72 h-72 rounded-full -z-10 blur-[14rem]"></div>
 			<div className="hidden lg:block absolute bottom-0 -left-1/4 bg-primary w-72 h-72 rounded-full -z-10 blur-[14rem]"></div>
 
-{/** Follow us */}
-<section>
+			{/** Follow us */}
+			<section>
 				<Wrapper className="flex flex-col items-center justify-center py-12 relative">
 					{/* <Container>
 						<LampContainer>
@@ -36,9 +36,10 @@ const Footer = () => {
 							</div>
 						</LampContainer>
 					</Container> */}
-					<Container className="relative z-[999999]">
+					<Container className="relative z-[999999] ">
 						<div className="flex items-center justify-center w-full -mt-40">
-							<div className="flex flex-col md:flex-row items-start md:items-center justify-start md:justify-between w-full px-4 md:px-8 rounded-lg lg:rounded-2xl border border-border/80 py-4 md:py-8">
+							<div className="flex md:flex-row items-start md:items-center justify-start md:justify-between w-full px-4 md:px-8 rounded-lg lg:rounded-2xl border border-border/80 py-4 md:py-8">
+								
 								<div className="flex flex-col items-start gap-4 max-w-md">
 									<div className="flex items-center gap-2 cursor-pointer group">
 										<h4 className="text-xl md:text-2xl font-semibold text-purple-400">
@@ -54,9 +55,8 @@ const Footer = () => {
 								</div>
 
 								{/* Socials */}
-								<div className="hidden md:flex relative items-center justify-center  w-full">
+								<div className="hidden md:flex relative items-center w-full mt-4">
 									<div className="flex items-center justify-center w-max rounded-full border-t border-foreground/30 bg-blue-950 backdrop-blur-lg px-2 py-1 md:py-2 gap-2 md:gap-8 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] shadow-background/40 select-none hover:shadow-background/60">
-									
 										<Link
 											href="https://t.me/@moneymesschannel"
 											className="hover:text-foreground/80"
@@ -83,34 +83,37 @@ const Footer = () => {
 										</Link>
 									</div>
 								</div>
+
+								{/* Created by Moneymess */}
+								<div className="">
+									<div className="flex flex-col items-left justify-start md:max-w-[200px]">
+										<div className="flex items-center ">
+											<Link href="/" className="flex items-center gap-2">
+												<OrbitIcon className="w6 h-6 ml-1 font-thin fill-100 text-white light-line-icon" />
+												<span className="text-lg font-black uppercase">
+													MoneyMess
+												</span>
+											</Link>
+										</div>
+										<span className="mt-4 text-neutral-200 text-sm flex items-center">
+											Created by &copy; Kitpes
+										</span>
+										<span className=" text-neutral-200 text-sm flex items-center">
+											with chill vibes
+										</span>
+									</div>
+								</div>
+								
 							</div>
+						</div>
+						<div className="border-t border-border/40 flex items-center justify-center">
+							<p className="text-sm text-muted-foreground text-center">
+								&copy; {new Date().getFullYear()} Kitpes. All rights reserved.
+							</p>
 						</div>
 					</Container>
 				</Wrapper>
 			</section>
-			
-			<div className="grid gap-12 xl:grid-cols-2 xl:gap-8">
-				<div className="flex flex-col items-left justify-start md:max-w-[200px]">
-					<div className="flex items-center ">
-						<Link href="/" className="flex items-center gap-2">
-							<OrbitIcon className="w6 h-6 ml-1 font-thin fill-100 text-white light-line-icon" />
-							<span className="text-lg font-black uppercase">MoneyMess</span>
-						</Link>
-					</div>
-					<span className="mt-4 text-neutral-200 text-sm flex items-center">
-						Created by &copy; Kitpes
-					</span>
-					<span className=" text-neutral-200 text-sm flex items-center">
-						with chill vibes
-					</span>
-				</div>
-
-				<div className="mt-4 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between ">
-					<p className="text-sm text-muted-foreground mt-8 md:mt-0">
-						&copy; {new Date().getFullYear()} Kitpes. All rights reserved.
-					</p>
-				</div>
-			</div>
 
 			{/* <div className="group relative overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200 flex items-center">
 					<div>
