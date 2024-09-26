473032b79ca12cb56887eeb498fc9716a804431c
Imit <156147322+imitfoundation@users.noreply.github.com>
2024-09-26 12:25:05 +0200

    add 4 gradients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,13 +80,13 @@ const HomePage = () => {
 			<section>
 				<Wrapper className='relative'>
 
-					<div className="absolute top-1/2 left-1/2 -z-10 gradient-attractiveness w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
+					<div className="absolute top-1/2 left-1/2 -z-10 gradient-popularity w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
 
 					<Container>
 						<div className="flex flex-col items-center justify-center py-20 h-full">
-							<div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-16 xl:col-span-2 justify-items-center items-center">
+							<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-16 xl:col-span-2 justify-items-center items-center">
 								
-								<div className="grid grid-cols-1 md:grid-cols-2 w-full gap-8">
+								<div className="">
 									<CardFlip 
 										srcFront="/cards/dominion.jpeg"
 										srcBack="/cards/card-back.jpg"
@@ -95,7 +95,9 @@ const HomePage = () => {
 										colorTo="#ddd"
 										colorFrom="#ff0f0f"
 									/>
-									<div className='px-6'>
+								</div>
+
+								<div className="w-[285px]">
 									<div className="mx-auto text-center pb-10">
 										<SectionBadge title="The Process" />
 										<h2 className="text-3xl lg:text-4xl font-black uppercase mt-6">
@@ -113,12 +115,9 @@ const HomePage = () => {
 										cum labore commodi ducimus quas accusantium rem, doloribus sapiente
 										voluptas ipsa error non accusamus sequi at.
 									</p>
-
-									</div>
-
 								</div>
 
-								<div className='px-6'>
+								<div className='col-span-1 md:col-span-2 px-6'>
 									<div className="max-w-lg mx-auto text-start md:text-center pb-10">
 										<SectionBadge title="The Process" />
 										<h2 className="text-3xl lg:text-4xl font-black uppercase mt-6">
